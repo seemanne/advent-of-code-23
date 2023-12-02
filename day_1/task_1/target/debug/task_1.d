1363e8dbb92e7001b1dec216fd3e7cefd60b334e
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-12-02 11:42:11 +0100

    Better ignore (#2)
    * add better gitignore
    
    * remove all objects
    
    * update gitignore

--- day_1/task_1/target/debug/task_1.d
+++
@@ -1,1 +0,0 @@
-/home/emanuel/Coding/Rust/advent-of-code-23/day_1/task_1/target/debug/task_1: /home/emanuel/Coding/Rust/advent-of-code-23/day_1/task_1/src/main.rs
--- day_1/task_2/target/debug/deps/task_1-d0a66bd2bd71d65d.d
+++
@@ -1,5 +0,0 @@
-/home/emanuel/Coding/Rust/advent-of-code-23/day_1/task_2/target/debug/deps/libtask_1-d0a66bd2bd71d65d.rmeta: src/main.rs
-
-/home/emanuel/Coding/Rust/advent-of-code-23/day_1/task_2/target/debug/deps/task_1-d0a66bd2bd71d65d.d: src/main.rs
-
-src/main.rs:
